4a6a5b90de78fc2f0dda318d045da2acdf81e9e6
pablo2637 <pabli2637@gmail.com>
2023-04-16 19:51:14 +0200

    routes admin y delete en detail

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,24 @@
+// @ts-nocheck
 document.addEventListener('DOMContentLoaded', () => {
 
     const divMenu = document.querySelector('#divMenu');
     const formPages = document.querySelector('.formPages');
     const selPage = document.querySelector('#page');
+    const divConfirm = document.querySelector('#divConfirm');
 
     document.addEventListener('click', ({ target }) => {
         // console.log('click', target)
         if (target.matches('i')) {
 
             if (target.id == 'nextPage') {
-                
-                selPage.value = parseInt(selPage.value) + 1;                
+
+                selPage.value = parseInt(selPage.value) + 1;
                 formPages.submit();
             }
 
             if (target.id == 'prevPage') {
-                
-                selPage.value = parseInt(selPage.value) - 1;                
+
+                selPage.value = parseInt(selPage.value) - 1;
                 formPages.submit();
             }
 
@@ -31,10 +33,23 @@ document.addEventListener('DOMContentLoaded', () => {
             else if (target.classList.contains('cancelDelete'))
                 showWarning(target);
 
+            else if (target.classList.contains('cancelEditDelete'))
+                divConfirm.classList.toggle('mostrarNav');
+
+
+        } else if (target.matches('button')) {
+
+            if (target.id == 'btnDelete')
+                divConfirm.classList.toggle('mostrarNav');
+
         } else {
 
             if (divMenu.classList.contains('mostrarNav'))
                 divMenu.classList.toggle('mostrarNav');
+
+            if (divConfirm)
+                if (divConfirm.classList.contains('mostrarNav'))
+                    divConfirm.classList.toggle('mostrarNav');
 
         }
 
